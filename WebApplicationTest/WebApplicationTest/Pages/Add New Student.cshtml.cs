7bb10ac3ad2cbde219ca194b296b291104901342
thenugamage <thenugamage311@gmail.com>
2024-11-05 01:24:20 +0530

    none
    none

---
+++ WebApplicationTest/WebApplicationTest/Pages/Add New Student.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationTest.Pages
+{
+    public class Add_New_StudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApplicationTest/WebApplicationTest/Pages/AddNewCourse.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationTest.Pages
+{
+    public class AddNewCourseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
